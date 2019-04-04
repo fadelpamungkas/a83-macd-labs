818856335ce961ce0d72d1c61e347ba3f27165be
fadelpamungkas <fadelpamungkas3@gmail.com>
2019-04-04 23:21:25 +0700

    Update create-table.sql

--- create-table.sql
+++ create-table.sql
@@ -2,6 +2,6 @@ create table [dbo].[Registration](
     id INT NOT NULL IDENTITY(1,1) PRIMARY KEY(id),
     name VARCHAR(30),
     email VARCHAR(30),
-    job VARCHAR(30),
+    phone VARCHAR(30),
     date DATE
 );
